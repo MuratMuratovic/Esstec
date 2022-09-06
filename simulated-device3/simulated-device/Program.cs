b5b665f2b415fb2c82680654d52535370f87b1d5
Murat Muratovic <108512704+MuratMuratovic@users.noreply.github.com>
2022-09-06 16:05:16 +0200

    first commit

---
+++ simulated-device3/simulated-device/Program.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// This application uses the Azure IoT Hub device SDK for .NET
+// For samples see: https://github.com/Azure/azure-iot-sdk-csharp/tree/master/iothub/device/samples
+
+using System;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simulated_device
+{
+
+    class SimulatedDevice
+    {
+
+        private static DeviceClient s_deviceClient;
+
+        // The device connection string to authenticate the device with your IoT hub.
+        // Using the Azure CLI:
+        // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
+        private readonly static string s_connectionString = "HostName=CamozziIOT.azure-devices.net;DeviceId=SystemContainer;SharedAccessKey=Q9QbFuZdsucqcgGtcXQ417SRbXSxJvZeuHCh49pNDUc=";
+
+        // Async method to send simulated telemetry
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+            int messageId = 1;
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+
+                var telemetryDataPoint = new
+                {
+                    messageId = messageId,
+                    deviceId = "SystemContainer",
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                messageId++;
+
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+                await s_deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sent message: {1}", DateTime.Now, messageString);
+                await Task.Delay(5000);
+            }
+        }
+        private static async void ReceiveC2dAsync()
+        {
+            while (true)
+            {
+                Message receivedMessage = await s_deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                await s_deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device. Press Ctrl-C to exit.\n");
+            // Connect to the IoT hub using the MQTT protocol
+            s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString, TransportType.Mqtt);
+            SendDeviceToCloudMessagesAsync();//send messages async function, occuring any time
+            ReceiveC2dAsync();//receive async, can occur any time during the operation of application
+
+            Console.ReadLine();
+        }
+    }
+}
